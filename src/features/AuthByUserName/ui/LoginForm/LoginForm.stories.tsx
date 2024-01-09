d236480801cf535fdd743facd291420c2ad37e32
Jimmy_K <dmit.karpovich@gmail.com>
2024-01-09 12:29:08 +0100

    recovered stories LoginForm meta

--- src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUserName/ui/LoginForm/LoginForm.stories.tsx
@@ -1,9 +1,18 @@
-import { Meta } from '@storybook/blocks';
+import { type Meta } from '@storybook/blocks';
 import type { StoryObj } from '@storybook/react';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { LoginForm } from './LoginForm';
+
+// const meta = {
+//     component: LoginForm,
+//     title: 'features/AuthByUserName/LoginForm',
+//     parameters: {
+//         layout: 'fullscreen'
+//     },
+//     tags: ['autodocs']
+// } satisfies Meta<typeof LoginForm>;
 
 const meta: Meta<typeof LoginForm> = {
     component: LoginForm,
