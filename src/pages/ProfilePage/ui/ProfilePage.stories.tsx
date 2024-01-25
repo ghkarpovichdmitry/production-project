df1b90fcd434b5f0a566aa42e17f36c3a595b18e
dmit.karpovich@gmail.com <im@Ge2you>
2024-01-25 18:22:21 +0100

    updated stories and added loki files

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import ProfilePage from './ProfilePage';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 
 const meta: Meta = {
     component: ProfilePage,
@@ -16,9 +17,27 @@ export default meta;
 type Story = StoryObj<typeof ProfilePage>;
 
 export const Normal: Story = {
-    decorators: [ThemeDecorator(Theme.LIGHT)]
+    decorators: [
+        ThemeDecorator(Theme.LIGHT),
+        StoreDecorator({
+            profile: {
+                data: undefined,
+                isLoading: false,
+                error: undefined,
+                readonly: true
+            }
+        })]
 };
 
 export const Dark: Story = {
-    decorators: [ThemeDecorator(Theme.DARK)]
+    decorators: [
+        ThemeDecorator(Theme.DARK),
+        StoreDecorator({
+            profile: {
+                data: undefined,
+                isLoading: false,
+                error: undefined,
+                readonly: true
+            }
+        })]
 };
